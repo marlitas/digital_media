c81c57ddf02774a913de97af2e7c36766405a9d8
Marla Schulz <marlaa.schulz@gmail.com>
2022-02-07 16:45:08 -0700

    Update navbar to responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,22 @@
 import './header.css'
-import {NavLink, Link} from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function header({ title }) {
   return (
-    <div className='nav-bar'>
-      <Link to='/'>
+    <Navbar collapseOnSelect expand="lg" variant="dark">
+      <Navbar.Brand href="/">
         <h1 className="site-title">{title}</h1>
-      </Link>
-      <div className="menu">
-        <NavLink activeClassName='active' to='/portfolios'>Portfolios</NavLink>
-        <NavLink activeClassName='active' to='/contact'>Contact</NavLink>
-        <NavLink activeClassName='active' to='/resources'>Resources</NavLink>
-      </div>
-    </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <NavLink activeClassName='active' to='/portfolios'>Portfolios</NavLink>
+          <NavLink activeClassName='active' to='/contact'>Contact</NavLink>
+          <NavLink activeClassName='active' to='/resources'>Resources</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
