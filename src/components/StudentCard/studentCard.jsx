a5c23b511030e8c1089fd37fbd785bc40d051336
Marla Schulz <marlaa.schulz@gmail.com>
2022-03-23 11:04:05 -0600

    Access student through name, complete fetch

--- src/components/StudentCard/studentCard.jsx
+++ src/components/StudentCard/studentCard.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 import './studentCard.css';
 
 function studentCard({ name, avatar, id }) {
     return (
-        <Link to={name}>
+        <Link to={{pathname: name, state: { params: {id: id}}}}>
             <div className="card">
                 <img src={avatar} className="avatar" />
                 <h2 className='card-heading'>{name}</h2>
--- src/components/portfolios/portfolio.jsx
+++ src/components/portfolios/portfolio.jsx
@@ -1,28 +1,30 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import studentCard from '../StudentCard/studentCard';
+import { useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import './portfolio.css';
 
-function portfolio() {
-    const getStudent = {
-        id: 1,
-        type: 'student',
-        attributes: {
-            name: 'Sparky',
-            avatar_url: 'https://picsum.photos/200',
-            code: 'another url',
-            major: 'dog',
-            enrolled: true,
-            about: 'I am a good boy',
-        }
-    }
+function Portfolio() {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [student, setStudent] = useState()
+    const name = useParams()
+    useEffect( () => {
+        fetch(`https://digital-media-api.herokuapp.com/api/v1/students/${name.name}`)
+                .then(response => response.json())
+                .then(
+                    (data) => {
+                        setIsLoaded(true);
+                        setStudent(data.data)
+                    }
+                )
+    })
 
     return (
         <Container>
-            <h2 className='subheading'>{getStudent.attributes.name}</h2>
-            <img src={getStudent.attributes.avatar_url}></img>
-            <p>{getStudent.attributes.about}</p>
+            <h2 className='subheading'>{student.attributes.name}</h2>
+            <img src={student.attributes.avatar_url}></img>
+            <p>{student.attributes.about}</p>
         </Container>
     )
 }
 
-export default portfolio
+export default Portfolio
